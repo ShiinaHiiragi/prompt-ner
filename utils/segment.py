8dfd323f4e1a4a95c4be789476955455edc05914
Shiina <shiinahiiragi@outlook.com>
2023-04-15 17:03:18 +0800

    add thulc for word segmentation

---
+++ utils/segment.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from .thulac import thulac
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+assert os.path.exists(os.path.join(current_path, "./thulac/models"))
+cutter = thulac(seg_only=True)
+
+def cut(sentence):
+    result = cutter.cut(sentence)
+    return list(map(lambda item: item[0], result))
