77deddf817304e429992cdf9247f2e526bb2a1cf
Shiina <shiinahiiragi@outlook.com>
2023-04-22 18:22:05 +0800

    verify for bart_test

--- utils/metrics.py
+++ utils/metrics.py
@@ -15,3 +15,12 @@ def calc_acc(predict, ans):
             correct += 1
 
     return correct / total
+
+def bart_calc_acc(predict, ans):
+    correct, total = 0, 0
+    for index in range(len(predict)):
+        for sub_index in range(len(predict[index])):
+            correct += 1 if predict[index][sub_index] == ans[index][sub_index] else 0
+            total += 1
+
+    return correct / total
