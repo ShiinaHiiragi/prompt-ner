e99d0ca777001d57a918999af2d495b99ae9b61d
Ichinoe <shiinahiiragi@outlook.com>
2023-04-29 17:12:00 +0800

    finish baseline

--- baseline_test.py
+++ baseline_test.py
@@ -7,9 +7,11 @@ from operators.NERDataset import NERDataset
 from operators.NERModel import NERModel
 
 DATASET_NAME = "msra"
+EPOCH_INDEX = 0
+
 tokenizer = tokenizer_loader(AutoTokenizer, "bert-base-chinese")
 test_dataset = NERDataset(tokenizer=tokenizer, reader=f"./data/{DATASET_NAME}.test")
-model = torch.load(f"./pretrained/model/fine-tune/baseline-{DATASET_NAME}-epoch-00.pt")
+model = torch.load(f"./pretrained/model/fine-tune/baseline-{DATASET_NAME}-epoch-{EPOCH_INDEX:02d}.pt")
 
 LOG("DATASET & MODEL LOADED")
 test_acc = baseline_test(test_dataset, model)
