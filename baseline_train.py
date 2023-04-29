f1768f185e21bc1508d72a2f485daf8f0225ce3e
Ichinoe <shiinahiiragi@outlook.com>
2023-04-29 16:55:24 +0800

    change name

--- baseline_train.py
+++ baseline_train.py
@@ -15,25 +15,25 @@ BATCH_SIZE = 4
 DATASET_NAME = "msra"
 
 train_reader = CONLLReader(f"./data/{DATASET_NAME}.train")
-dev_reader = CONLLReader(f"./data/{DATASET_NAME}.lite.dev")
+dev_lite_reader = CONLLReader(f"./data/{DATASET_NAME}.lite.dev")
 tokenizer = tokenizer_loader(AutoTokenizer, "bert-base-chinese")
 
 # test_reader = CONLLReader(f"./data/{DATASET_NAME}.test")
-# assert train_reader.domain == dev_reader.domain
+# assert train_reader.domain == dev_lite_reader.domain
 # assert train_reader.domain == test_reader.domain
 
 LOG("READER & TOKENIZER LOADED")
 train_dataset = NERDataset(tokenizer=tokenizer, reader=train_reader)
-dev_dataset = NERDataset(tokenizer=tokenizer, reader=dev_reader)
+dev_lite_dataset = NERDataset(tokenizer=tokenizer, reader=dev_lite_reader)
 model = NERModel(train_dataset.num_labels, bert_model="bert-base-chinese")
 
-def train_loop(train_dataset, dev_dataset, model):
+def train_loop(train_dataset, dev_lite_dataset, model):
     optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
     for index in range(EPOCH):
-        train(train_dataset, dev_dataset, model, optimizer)
+        train(train_dataset, dev_lite_dataset, model, optimizer)
         model.save_pretrained(f"./pretrained/model/fine-tune/baseline-{DATASET_NAME}-epoch{index:02d}.pt")
 
-def train(train_dataset, dev_dataset, model, optimizer):
+def train(train_dataset, dev_lite_dataset, model, optimizer):
     dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE)
     for batch_index, (batch_X, batch_Y) in enumerate(tqdm(dataloader)):
         _, __, loss = model(batch_X, batch_Y)
@@ -41,8 +41,8 @@ def train(train_dataset, dev_dataset, model, optimizer):
         loss.backward()
         optimizer.step()
         if batch_index > 0 and batch_index % 100 == 0:
-            dev_acc = baseline_test(dev_dataset, model)
+            dev_acc = baseline_test(dev_lite_dataset, model)
             LOG(f"\nDEV ACC: {dev_acc}")
 
 LOG("DATASET & MODEL LOADED")
-train_loop(train_dataset, dev_dataset, model)
+train_loop(train_dataset, dev_lite_dataset, model)
