6d8fc46ae680ba12398854dc1724bc46c69f5f06
Ichinoe <shiinahiiragi@outlook.com>
2023-04-29 18:51:54 +0800

    change prompts

--- PromptWeaver.py
+++ PromptWeaver.py
@@ -9,11 +9,18 @@ class BartPromptOperator(PromptOperator):
     POSITIVE_TEMPLATE = "“{candidate_span}”是一个{entity_type}实体"
     NEGATIVE_TEMPLATE = "“{candidate_span}”不是一个命名实体"
 
+    # 实体：POSITIVE_RATIO 概率可以保留
+    # 非实体：NEGATIVE_RATIO 概率可以保留
+    # 　　　　非实体是实体的 POSITIVE_NEGATIVE_RATIO 倍，最低为 NEGATIVE_LOWER_BOND(ratio) 个
+    # 　　　　该函数有 ratio 概率输出 1，有 1 - ratio 概率输出 0，默认为 DEFAULT_LOWER_BOND
+    POSITIVE_RATIO = 1
+    NEGATIVE_RATIO = 1
+    POSITIVE_NEGATIVE_RATIO = 1
+    DEFAULT_LOWER_BOND = 0.25
     NEGATIVE_LOWER_BOND = lambda self, ratio: 0 if random() > ratio else 1
 
-    def __init__(self, reader, ratio=0.75):
+    def __init__(self, reader):
         super().__init__(reader)
-        self.ratio = ratio
 
     def __generate_golden_entity(self, sentence, label):
         golden_entity = []
@@ -45,10 +52,13 @@ class BartPromptOperator(PromptOperator):
         entities = list(map(lambda item: item[0], golden_entity))
         self.__clear_invalid_words(words, entities)
 
-        negative_count = max(self.NEGATIVE_LOWER_BOND(0.25), round(len(entities) * self.ratio))
+        negative_count = max(
+            self.NEGATIVE_LOWER_BOND(self.DEFAULT_LOWER_BOND),
+            round(len(entities) * self.POSITIVE_NEGATIVE_RATIO)
+        )
+
         while negative_count < len(words):
             words.pop(randint(0, len(words) - 1))
-
         return words
 
     def _format(self, sentence, label):
@@ -58,10 +68,14 @@ class BartPromptOperator(PromptOperator):
         negative_entity = self.__generate_negative_entity(sentence, label, golden_entity)
 
         for item, tag in golden_entity:
+            if random() > self.POSITIVE_RATIO:
+                continue
             positive_format = self.POSITIVE_TEMPLATE.format(candidate_span=item, entity_type=LABEL_ENTITY[tag])
             result.append(f"{sentence_str}\t{positive_format}")
 
         for item in negative_entity:
+            if random() > self.NEGATIVE_RATIO:
+                continue
             negative_format = self.NEGATIVE_TEMPLATE.format(candidate_span=item)
             result.append(f"{sentence_str}\t{negative_format}")
 
@@ -72,8 +86,7 @@ class EntailPromptOperator(PromptOperator):
     NEGATIVE_FLAG = "错"
     MASK_FLAG = "[MASK]"
 
-    # true positive:  「韩国」是地名实体
-    # true negative:  「城市」不是命名实体
+
     TRUE_TEMPLATE = {
         "test_positive": "“{candidate_span}”是一个{entity_type}实体。" + MASK_FLAG,
         "true_positive": "“{candidate_span}”是一个{entity_type}实体。" + POSITIVE_FLAG,
@@ -81,24 +94,36 @@ class EntailPromptOperator(PromptOperator):
         "true_negative": "“{word_span}”不是一个命名实体。" + POSITIVE_FLAG
     }
 
-    # false positive: 「城市」是地名实体
-    # false positive: 「韩国」是人名实体
-    # false negative: 「韩国」不是命名实体
+
     FALSE_TEMPLATE = {
         "false_positive_non": "“{word_span}”是一个{random_entity_type}实体。" + NEGATIVE_FLAG,
         "false_positive_entity": "“{candidate_span}”是一个{another_entity_type}实体。" + NEGATIVE_FLAG,
         "false_negative": "“{candidate_span}”不是一个命名实体。" + NEGATIVE_FLAG
     }
 
-    NEGATIVE_LOWER_BOND = lambda self, ratio: 0 if random() > ratio else 1
+    # true positive:  「韩国」是地名实体
+    # true negative:  「城市」不是命名实体
+    # false positive: 「城市」是地名实体
+    # false positive: 「韩国」是人名实体
+    # false negative: 「韩国」不是命名实体
 
+    # TP：实体，TRUE_POSITIVE_RATIO 概率保留
+    # TN：非实体，TRUE_NEGATIVE_RATIO 概率保留
+    # FP：分为实体与非实体（与 TN 独立筛选）部分，两者均有 FALSE_POSITIVE_RATIO 概率保留
+    # FN：实体，FALSE_NEGATIVE_RATIO 概率保留
+    # 非实体：非实体是实体的 POSITIVE_NEGATIVE_RATIO 倍，最低为 NEGATIVE_LOWER_BOND(ratio) 个
+    # 　　　　该函数有 ratio 概率输出 1，有 1 - ratio 概率输出 0，默认为 DEFAULT_LOWER_BOND
     TRUE_POSITIVE_RATIO = 1
+    TRUE_NEGATIVE_RATIO = 0.25
     FALSE_POSITIVE_RATIO = 0.25
     FALSE_NEGATIVE_RATIO = 0.25
 
-    def __init__(self, reader, ratio=0.5):
+    POSITIVE_NEGATIVE_RATIO = 0.5
+    DEFAULT_LOWER_BOND = 0.25
+    NEGATIVE_LOWER_BOND = lambda self, ratio: 0 if random() > ratio else 1
+
+    def __init__(self, reader):
         super().__init__(reader)
-        self.ratio = ratio
 
     def __generate_golden_entity(self, sentence, label):
         golden_entity = []
@@ -130,10 +155,13 @@ class EntailPromptOperator(PromptOperator):
         entities = list(map(lambda item: item[0], golden_entity))
         self.__clear_invalid_words(words, entities)
 
-        negative_count = max(self.NEGATIVE_LOWER_BOND(0.25), round(len(entities) * self.ratio))
+        negative_count = max(
+            self.NEGATIVE_LOWER_BOND(self.DEFAULT_LOWER_BOND),
+            round(len(entities) * self.POSITIVE_NEGATIVE_RATIO)
+        )
+
         while negative_count < len(words):
             words.pop(randint(0, len(words) - 1))
-
         return words
 
     def _format(self, sentence, label):
@@ -156,6 +184,8 @@ class EntailPromptOperator(PromptOperator):
 
         # true negative
         for item in true_negative_entity:
+            if random() > self.TRUE_NEGATIVE_RATIO:
+                continue
             negative_format = self.TRUE_TEMPLATE["true_negative"].format(word_span=item)
             result.append(f"{sentence_str}{negative_format}")
 
@@ -163,6 +193,8 @@ class EntailPromptOperator(PromptOperator):
         entities_size = len(LABEL_ENTITY)
         entities_name = list(LABEL_ENTITY.values())
         for item in false_positive_entity:
+            if random() > self.FALSE_POSITIVE_RATIO:
+                continue
             rand_index = randint(0, entities_size - 1)
             positive_format = self.FALSE_TEMPLATE["false_positive_non"].format(
                 word_span=item,
@@ -174,7 +206,6 @@ class EntailPromptOperator(PromptOperator):
         for item, tag in golden_entity:
             if random() > self.FALSE_POSITIVE_RATIO:
                 continue
-
             copy_entities_name = entities_name.copy()
             copy_entities_name.pop(copy_entities_name.index(LABEL_ENTITY[tag]))
 
