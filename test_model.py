a06a2e0e7d3913ba868315b990c7c7ea07ca04e0
Ichinoe <shiinahiiragi@outlook.com>
2023-05-02 15:32:53 +0800

    add prompt_bart_train

--- test_model.py
+++ test_model.py
@@ -19,5 +19,16 @@ def test_bert_base_chinese():
     mask = pipeline("fill-mask", tokenizer=tokenizer, model=model)
     return mask("巴黎是[MASK]国的首都。")
 
-ans = test_bert_base_chinese()
+def test_bart_base_chinese():
+    from transformers import BertTokenizer, BartForConditionalGeneration
+    from transformers import pipeline
+    from utils.saver import tokenizer_loader, model_loader
+
+    tokenizer = tokenizer_loader(BertTokenizer, "fnlp/bart-base-chinese")
+    model = model_loader(BartForConditionalGeneration, "fnlp/bart-base-chinese")
+
+    generator = pipeline("text2text-generation", tokenizer=tokenizer, model=model)
+    return generator("北京是[MASK]的首都")
+
+ans = test_bart_base_chinese()
 pprint(ans)
