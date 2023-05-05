5538cdd806a371397c5ab5f3ba819dd23854a1cc
Ichinoe <ShiinaHiiragi@outlook.com>
2023-05-05 17:08:51 +0000

    change sequence of init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ echo "download Git Graph"
 git config --global user.name "Ichinoe"
 git config --global user.email "ShiinaHiiragi@outlook.com"
 
+pip install simpletransformers
+
+python utils/saver.py
 python PromptWeaver.py --dataset=msra --pclass=bart --suffix=train
 python PromptWeaver.py --dataset=msra --pclass=entail --suffix=train
-python utils/saver.py
-
-pip install simpletransformers
