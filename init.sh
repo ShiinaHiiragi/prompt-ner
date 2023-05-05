5eb9bc5a5e7bc92e003ccfcecfb923fa6ae285f4
Ichinoe <ShiinaHiiragi@outlook.com>
2023-05-05 17:13:04 +0000

    add highlight for init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-echo "copy utils/thulac/models"
-echo "download Git Graph"
+echo -e "\033[1;31m copy utils/thulac/models \033[0m"
+echo -e "\033[1;31m download Git Graph \033[0m"
 
 git config --global user.name "Ichinoe"
 git config --global user.email "ShiinaHiiragi@outlook.com"
