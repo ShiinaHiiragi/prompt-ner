b52fb65773c329539230106dd20e018bac96f49a
Ichinoe <ShiinaHiiragi@outlook.com>
2023-05-05 11:30:01 +0000

    change gram count

--- utils/constants.py
+++ utils/constants.py
@@ -9,7 +9,7 @@ PAD_TOKEN = 0
 SPECIAL_TOKENS = [CLS_TOKEN, SEP_TOKEN, PAD_TOKEN]
 SPECIAL_ID = -1
 
-GRAM = 4
+GRAM = 6
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 LOG = lambda *msg: [print(f"\033[1;31m{it}\033[0m") for it in msg]
 
