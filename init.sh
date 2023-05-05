231eb31bca225d010aa2cab05d1c93f2cd23e053
Ichinoe <ShiinaHiiragi@outlook.com>
2023-05-05 06:08:13 +0000

    add init for docker container

---
+++ init.sh
@@ -0,0 +1,8 @@
+echo "copy utils/thulac/models"
+echo "download Git Graph"
+
+git config --global user.name "Ichinoe"
+git config --global user.email "ShiinaHiiragi@outlook.com"
+
+pip install simpletransformers
+python utils/saver.py
